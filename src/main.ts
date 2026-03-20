5cc3829a5f13bc27e0555f9875d3072ceef1c7a9
lonewolf <kkong8387@gmail.com>
2026-03-20 08:55:38 +0000

    200326 a

--- src/main.ts
+++ src/main.ts
@@ -24,58 +24,58 @@ async function bootstrap() {
     })
   );
   app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-  
+  app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
 
   // ✅ Global response interceptor
   app.useGlobalInterceptors(new ResponseInterceptor());
 
-app.enableCors({
-  origin: "*",
-  methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-});
+  app.enableCors({
+    origin: "*",
+    methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  });
 
-// app.enableCors({
-//   origin: [
-//     "https://hohoe.test.smartcitygh.com",
-//     "https://dev.rev-collect.smartcitygh.com",
-//     "https://hohoe.smartcitygh.com",
-//     "https://api.staging.rev-collect.smartcitygh.com", 
-    
-//     "*"
-//   ],
-//   methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-// });
+  // app.enableCors({
+  //   origin: [
+  //     "https://hohoe.test.smartcitygh.com",
+  //     "https://dev.rev-collect.smartcitygh.com",
+  //     "https://hohoe.smartcitygh.com",
+  //     "https://api.staging.rev-collect.smartcitygh.com", 
+
+  //     "*"
+  //   ],
+  //   methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  // });
 
 
 
-const logService = app.get(ActivityLogsService);
-app.useGlobalInterceptors(
-  new ActivityLogInterceptor(logService),
-);
+  const logService = app.get(ActivityLogsService);
+  app.useGlobalInterceptors(
+    new ActivityLogInterceptor(logService),
+  );
 
-  
+
 
   const config = new DocumentBuilder()
     .setTitle('Voter API')
     .setDescription('API to support Voter app')
     .setVersion('1.0')
     .addBearerAuth(
-    {
-      type: 'http',
-      scheme: 'bearer',
-      bearerFormat: 'JWT',
-      in: 'header',
-    },
-    'access-token',
-  )
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+      },
+      'access-token',
+    )
     .addTag('exits')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  console.log('NODE_ENV', nodeEnv); 
+  console.log('NODE_ENV', nodeEnv);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
